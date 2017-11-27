588b5aae70fdb998817a7af4b16d81832273074a
Sabari <sabarinathan94@gmail.com>
2017-11-27 17:16:33 +0000

    Update snif.py -> extracts IP and Port number and prints only them

--- src/MT2016120_Sabari_Nathan/snif.py
+++ src/MT2016120_Sabari_Nathan/snif.py
@@ -1,14 +1,56 @@
 #!/usr/bin/evn python
-from scapy.all import *
-def print_summary(pkt):
-    if IP in pkt:
-        ip_src=pkt[IP].src
-        ip_dst=pkt[IP].dst
-    if TCP in pkt:
-        tcp_sport=pkt[TCP].sport
-        tcp_dport=pkt[TCP].dport
-
-        print " IP src " + str(ip_src) + " TCP sport " + str(tcp_sport) 
-        print " IP dst " + str(ip_dst) + " TCP dport " + str(tcp_dport)
-
-sniff(filter="ip",prn=print_summary)
+#Packet sniffer in python for Linux
+#Sniffs only incoming TCP packet
+ 
+import socket, sys
+from struct import *
+ 
+#create an INET, STREAMing socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+except socket.error , msg:
+    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+ 
+# receive a packet
+while True:
+    packet = s.recvfrom(65565)
+     
+    #packet string from tuple
+    packet = packet[0]
+     
+    #take first 20 characters for the ip header
+    ip_header = packet[0:20]
+     
+    #now unpack them :)
+    iph = unpack('!BBHHHBBH4s4s' , ip_header)
+     
+    version_ihl = iph[0]
+    version = version_ihl >> 4
+    ihl = version_ihl & 0xF
+     
+    iph_length = ihl * 4
+     
+    ttl = iph[5]
+    protocol = iph[6]
+    s_addr = socket.inet_ntoa(iph[8]);
+    d_addr = socket.inet_ntoa(iph[9]);
+     
+#IP Packet print over here
+     
+    tcp_header = packet[iph_length:iph_length+20]
+     
+    #now unpack them :)
+    tcph = unpack('!HHLLBBHHH' , tcp_header)
+     
+    source_port = tcph[0]
+    dest_port = tcph[1]
+    sequence = tcph[2]
+    acknowledgement = tcph[3]
+    doff_reserved = tcph[4]
+    tcph_length = doff_reserved >> 4
+    if(dest_port != 22): 
+	print 'Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr) + 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port)# + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
+     
+    h_size = iph_length + tcph_length * 4
+    data_size = len(packet) - h_size
