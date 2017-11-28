99d9ac574eb177958fbef77fb4d444167953fcc0
Sabari <sabarinathan94@gmail.com>
2017-11-28 10:15:58 +0000

    Replaced snif.py with interface level sniffer -> One code for all interfaces

--- src/MT2016120_Sabari_Nathan/snif.py
+++ src/MT2016120_Sabari_Nathan/snif.py
@@ -1,56 +1,19 @@
 #!/usr/bin/evn python
-#Packet sniffer in python for Linux
-#Sniffs only incoming TCP packet
- 
-import socket, sys
-from struct import *
- 
-#create an INET, STREAMing socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-except socket.error , msg:
-    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
- 
-# receive a packet
+from scapy.all import *
+#list to store ip&port#match
+list = []
 while True:
-    packet = s.recvfrom(65565)
-     
-    #packet string from tuple
-    packet = packet[0]
-     
-    #take first 20 characters for the ip header
-    ip_header = packet[0:20]
-     
-    #now unpack them :)
-    iph = unpack('!BBHHHBBH4s4s' , ip_header)
-     
-    version_ihl = iph[0]
-    version = version_ihl >> 4
-    ihl = version_ihl & 0xF
-     
-    iph_length = ihl * 4
-     
-    ttl = iph[5]
-    protocol = iph[6]
-    s_addr = socket.inet_ntoa(iph[8]);
-    d_addr = socket.inet_ntoa(iph[9]);
-     
-#IP Packet print over here
-     
-    tcp_header = packet[iph_length:iph_length+20]
-     
-    #now unpack them :)
-    tcph = unpack('!HHLLBBHHH' , tcp_header)
-     
-    source_port = tcph[0]
-    dest_port = tcph[1]
-    sequence = tcph[2]
-    acknowledgement = tcph[3]
-    doff_reserved = tcph[4]
-    tcph_length = doff_reserved >> 4
-    if(dest_port != 22): 
-	print 'Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr) + 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port)# + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
-     
-    h_size = iph_length + tcph_length * 4
-    data_size = len(packet) - h_size
+	#a = sniff(iface=["s1-eth1","s1-eth2"], count=1)
+	a = sniff(iface=["s1-eth1","s1-eth2"], prn=lambda x: x.sniffed_on+": "+x.summary())
+	for pkt in a:
+		if(pkt.haslayer(IP)):
+			ip_src = pkt.getlayer(IP).src
+			ip_dst = pkt.getlayer(IP).dst
+			ip_pair = ip_src + ' ' + ip_dst
+			if(ip_pair not in list):		
+				list.append(ip_pair)
+				#replace this with MySQL connection
+				print (ip_pair)
+		if (pkt.haslayer(TCP)):
+        		tcp_sport=pkt[TCP].sport
+        		tcp_dport=pkt[TCP].dport
